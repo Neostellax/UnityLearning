c5afa34fa4d184d75653c8321d29f89e16cbbb51
Neostellax <neosaw314159@gmail.com>
2024-03-14 19:05:14 +0800

    add Graph

--- Basics/Assets/Scripts/Graph.cs
+++ Basics/Assets/Scripts/Graph.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,18 +9,29 @@ public class Graph : MonoBehaviour
     Transform pointPrefab = default;
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+    Transform[] points;
 
     void Awake() {
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++){
-            Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;
+
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++){
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;            
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+        }
+    }
+
+    void Update() {
+        for (int i = 0; i < points.Length; i++){
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
         }
     }
 }
